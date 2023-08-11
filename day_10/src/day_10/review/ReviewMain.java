af17a1cb3d3ae5791d22ef691dcf68ddd7d842fa
DESKTOP-HP2CTIN\hbi <funkyplayer95@kakao.com>
2023-08-11 17:54:00 +0900

    [ADD] JAVA 0811 개념정리 -> 함수실행에 따른 결과 다시보기

---
+++ day_10/src/day_10/review/ReviewMain.java
@@ -0,0 +1,20 @@
+package day_10.review;
+
+public class ReviewMain {
+
+	public static void main(String[] args) {
+		// 변수
+		// 정수 변수 세 개를 선언 : 데이터타입 변수명
+		// 선언된 세 개의 변수 중 두번째 변수에 임의의 값으로 할당
+		
+//		int a ; int b = 2; int c;
+//		
+//		
+//		// 세 개의 변수를 출력
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+
+	}
+
+}
---
+++ day_10/src/day_10/review/Test.java
@@ -0,0 +1,14 @@
+package day_10.review;
+
+public class Test {
+	// 변수 영역 - 필드(속성)영역
+	// 함수 영역 - 메소드 영역
+	// 생성자 : 기본/인자있는 생성자
+	public Test() {}
+	
+	// Getters / Setters 메소드 - OOP에서의 Data Encryption
+	// 일반 메소드 = 정적/동적 메소드 
+	// 정적 메소드
+	// 동적 메소드
+	
+}
