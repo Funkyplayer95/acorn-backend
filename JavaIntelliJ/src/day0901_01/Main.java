683ad9fbbf22ebb32e9eb63f9e34b2e306ffcba9
DESKTOP-HP2CTIN\hbi <funkyplayer95@kakao.com>
2023-09-01 17:43:19 +0900

    [ADD] JAVA 230901 자바의정석 독서

---
+++ JavaIntelliJ/src/day0901_01/Main.java
@@ -0,0 +1,12 @@
+package day0901_01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("입력대기중입니다.");
+        String sInputVal = sc.nextLine();
+        System.out.println(sInputVal);
+    }
+}
