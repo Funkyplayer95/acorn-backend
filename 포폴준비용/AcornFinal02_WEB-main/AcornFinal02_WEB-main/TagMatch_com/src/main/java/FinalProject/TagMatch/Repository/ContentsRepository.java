f6656c5b77dc444a311c63b5c7fea1b6add52872
Funkyplayer95 <135031901+Funkyplayer95@users.noreply.github.com>
2023-12-25 00:37:46 +0900

    Delete 포폴준비용/AcornFinal02_WEB-main/AcornFinal02_WEB-main directory

--- 포폴준비용/AcornFinal02_WEB-main/AcornFinal02_WEB-main/TagMatch_com/src/main/java/FinalProject/TagMatch/Repository/ContentsRepository.java
+++
@@ -1,9 +0,0 @@
-//package FinalProject.TagMatch.Repository;
-//
-//import FinalProject.TagMatch.Entity.Contents;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ContentsRepository extends JpaRepository<Contents, String> {
-//
-//
-//}
